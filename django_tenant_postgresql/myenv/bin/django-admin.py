192e6c36d97f394da6bca7a254bc03d5d66b8a2e
estebanaldana <dantes-_inferno@htomail.com>
2019-07-15 04:31:02 -0500

    Crate project SaaS with Django Tenant/posgresql-session2-create news tenants

---
+++ django_tenant_postgresql/myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/mac/Documents/udemy cursos/django_tenant_postgresql/myenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
